3c1b1004787042d3dcfc679e66d7270ed15ed87c
Dmitry Barabanov <docbrayn008@mail.ru>
2021-04-27 11:43:51 +0500

    Документация

--- html/navtreedata.js
+++ html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "C++ sockets on Unix and Windows", "index.html", [
+  [ "SE_Lab08", "index.html", [
     [ "Классы", "annotated.html", [
       [ "Классы", "annotated.html", "annotated_dup" ],
       [ "Алфавитный указатель классов", "classes.html", null ],
